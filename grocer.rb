52e586c2a4f168d6941d66fe262adaac6f225218
adriennem76 <adrienne.c.miranda@gmail.com>
2019-06-21 17:03:33 -0400

    started

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,19 @@
 def consolidate_cart(cart)
   # code here
+  consolidated_cart = {}
+  
+  cart.each do |item_hash|
+    item_hash.each do |item, info_hash|
+      if consolidated_cart[item]
+        consolidated_cart[item][:count] += 1
+      else
+        consolidated_cart[item] = info_hash
+        consolidated_cart[item][:count] = 1
+      end
+    end
+  end
+  
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
